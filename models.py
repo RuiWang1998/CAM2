b5ba88da309ce881913192be34c86ee933672337
Rui Wang <ruiwang1998@outlook.com>
2019-01-31 00:15:02 -0500

    dim

--- models.py
+++ models.py
@@ -26,7 +26,7 @@ class ImageProcessor(nn.Module):
         )
 
         self.out = nn.Sequential(
-            nn.Linear(192, BOTTLENEK//2),
+            nn.Linear(300, BOTTLENEK//2),
             nn.LeakyReLU(0.2),
             nn.Linear(BOTTLENEK//2, BOTTLENEK//2),
             nn.LeakyReLU(0.2),
