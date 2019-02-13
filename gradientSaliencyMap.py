a8ef8bd77537c4367a2ad6bba9c96636a827378a
Rui Wang <32067492+RuiWang1998@users.noreply.github.com>
2019-02-13 13:43:16 -0500

    Some issues with suppressing

--- gradientSaliencyMap.py
+++ gradientSaliencyMap.py
@@ -3,7 +3,8 @@
 # This is my implementation of saliency map that figures out how much each of the 
 # pixels matters in the process of YOLOv3
 
-#%%
+#%% [markdown]
+# # Importing modules
 import torch
 import torch.nn as nn
 import torchvision
@@ -13,7 +14,7 @@ import numpy as np
 
 #%% [markdown]
 # # constants
-device = 'cpu' #torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 image_size = 416
 config_path = 'C:/Users/ruiwa/Documents/GitHub/repos/ClonedModel/PyTorch-YOLOv3/config/yolov3.cfg'
 weight_path = "C:/Users/ruiwa/Documents/GitHub/repos/ClonedModel/PyTorch-YOLOv3/weights/yolov3.weights"
@@ -46,75 +47,13 @@ img_detections = []
 #%%
 for batch_i, (img_paths, input_imgs) in enumerate(dataloader):
     # Configure input
-    input_imgs.requires_grad = True
     imgs.append(input_imgs)
 
-#%% [markdown]
-# # A simple demonstration
-# Here the result output would be of 7 dimensions. The first four are for the coordinates of the bounding boxes' angles, 
-# the fifth is the confidence that there is an object, the sixth is the confidence that the object belongs to that class,
-# which is the last output
-
-#%%
-input_imgs = imgs[0].to(device)
-input_imgs = Variable(input_imgs.data, requires_grad=True)
-output = model(input_imgs)
-
-#%% [markdown]
-# ## The output of the model is of shape (batch_size, num_boxes, num_classes + 5)
-# ###
-# #### For each of the bounding boxes, the first four is the spatial information of the box
-# #### and the fifth element is the confidence of this bounding box. THe rest of the output
-# #### should be the probability of each of the class that this box is predicting
-
-#%% [markdown]
-# ### this code below gives which of the boxes have a confidence level above 0.9
-confidence_mask = output[:, :, 4] > 0.9
-confidence_index = confidence_mask.nonzero()
-confidence_index = [entry[1] for entry in confidence_index]
-confidence_preds, class_preds = torch.max(output[confidence_mask][:,5:85], dim=1)
-conf_class = {conf:classes[class_preds[i]] for i,conf in enumerate(confidence_preds)}
-class_conf = {class_id:[] for class_id in conf_class.values()}
-for conf, class_item in conf_class.items():
-    class_conf[class_item].append(conf)
-class_conf = {class_id:max(value) for class_id, value in class_conf.items()}
-
-#%%
-confidences = output[:, :, 4][0][confidence_index[0]]
-
-#%% [markdown]
-# ### Let's first try to find out what the first box predicts
-class_conf, class_pred = torch.max(output[:, confidence_index[0], 5:85], dim=1)
-classes[class_pred]
-
-#%%
-class_conf.backward()
-
-#%%
-
-
-#%%
-saliency = input_imgs.grad[0].cpu().permute(1,2,0)
-
-#%% [markdown]
-# # The Saliency Map
-# ### Below are some mathematical operations that help visualization better
-saliency_up = torch.abs(saliency) * 1e7
-saliency_normed = (saliency_up - saliency_up.min())/(saliency_up.max() - saliency_up.min())
-saliency_normed_scaled = (saliency_normed + 0.7)**2
-saliency_scaled_nonlinear = torch.log(saliency_normed_scaled)
-saliency_pic = (saliency_scaled_nonlinear - saliency_scaled_nonlinear.min())/(saliency_scaled_nonlinear.max() - saliency_scaled_nonlinear.min())
-plt.imshow(saliency_pic)
-
-#%% [markdown]
-# The actual image
-plt.imshow(input_imgs[0].permute(1,2,0).detach())
 
 #%% [markdown]
 # # Let's organize and set this into a function
 def img2Saliency(input_imgs, threshold=0.9):
     input_imgs = input_imgs.to(device)
-    input_imgs = Variable(input_imgs.data, requires_grad=True)
     output = model(input_imgs)
 
     confidence_mask = output[:, :, 4] > threshold
@@ -126,9 +65,16 @@ def img2Saliency(input_imgs, threshold=0.9):
         class_conf[class_item].append(conf)
     class_conf = {class_id:max(value) for class_id, value in class_conf.items()}
 
-    saliency_maps = []
+    columns = 1
+    rows = len(class_conf) + 1
+    fig=plt.figure(figsize=(30, 30))
+
     for i, name in enumerate(class_conf.keys()):
+        # Each single time we need to clear the gradients or else it will carry on to the next saliency map
+        input_imgs.requires_grad=True
+        if input_imgs.grad is not None: input_imgs.grad.data.zero_()
         output2 = model(input_imgs)
+        suppressed = non_max_suppression(output2, 80, threshold)
         confidence_mask2 = output2[:, :, 4] > threshold
         confidence_preds2, class_preds2 = torch.max(output2[confidence_mask2][:,5:85], dim=1)
         conf_class2 = {conf:classes[class_preds2[i]] for i,conf in enumerate(confidence_preds2)}
@@ -136,35 +82,28 @@ def img2Saliency(input_imgs, threshold=0.9):
         for conf, class_item in conf_class2.items():
             class_conf2[class_item].append(conf)
         class_conf2 = {class_id:max(value) for class_id, value in class_conf2.items()}
-        class_conf2[name].backward()
-        print(name)
         
+        print(name)
+        print(class_conf2[name])
+
+        class_conf2[name].backward()
         saliency = input_imgs.grad[0].cpu().permute(1,2,0)
 
         saliency_up = torch.abs(saliency) * 1e7
         saliency_normed = (saliency_up - saliency_up.min())/(saliency_up.max() - saliency_up.min())
-        saliency_normed_scaled = (saliency_normed + 0.7)**2
+        saliency_normed_scaled = (saliency_normed + 0.3)**2
         saliency_scaled_nonlinear = torch.log(saliency_normed_scaled)
         saliency_pic = (saliency_scaled_nonlinear - saliency_scaled_nonlinear.min())/(saliency_scaled_nonlinear.max() - saliency_scaled_nonlinear.min())
-        saliency_maps.append(saliency_pic)
 
-    columns = 1
-    rows = len(class_conf) + 1
-    fig=plt.figure(figsize=(8, 8))
-    for i in range(1, columns*rows):
-        img = saliency_maps[i - 1]
-        fig.add_subplot(rows, columns, i)
-        plt.imshow(img)
+        fig.add_subplot(rows, columns, i + 1)
+        plt.imshow(saliency_pic)
 
     fig.add_subplot(rows, columns, rows)
-    plt.imshow(input_imgs[0].permute(1,2,0).detach())
+    plt.imshow(input_imgs[0].cpu().permute(1,2,0).detach())
     plt.show()
 
-
 #%%
-img2Saliency(imgs[0])
+img2Saliency(imgs[4])
 
 #%%
 plt.imshow(imgs[1][0].permute(1,2,0).detach().numpy())
-
-#%%
